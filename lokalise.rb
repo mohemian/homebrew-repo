4efb49c9cc38e53f1039c4cb03dda25a06784233
carol-mohemian <carol.capek@mohemian.com>
2017-07-12 14:20:42 +0200

    Update hash (#3)

--- lokalise.rb
+++ lokalise.rb
@@ -6,7 +6,7 @@ class Lokalise < Formula
   homepage ""
   url "https://lokalise.co/cli/lokalise-0.411-darwin-amd64.tgz"
   version "1.0.0"
-  sha256 "11d547a95e257a17bf0b80bdca6e3a51475a580671db7b597c636f5aa8bf1d58"
+  sha256 "e11eb940ad2fa3a9418afb304c3a617576bc45d875ea95ffc2119991c4fc24f7"
 
     def install
         bin.install "lokalise"
