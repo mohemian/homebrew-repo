ccd3a395304dcf48b167c041704ad10818da367c
carol-mohemian <carol.capek@mohemian.com>
2017-07-12 13:27:12 +0200

    Update download url

--- lokalise.rb
+++ lokalise.rb
@@ -4,7 +4,7 @@
 class Lokalise < Formula
   desc ""
   homepage ""
-  url "https://lokalise.co/cli/lokalise-0.2b-darwin-amd64.tgz"
+  url "https://lokalise.co/cli/lokalise-0.411-darwin-amd64.tgz"
   version "1.0.0"
   sha256 "11d547a95e257a17bf0b80bdca6e3a51475a580671db7b597c636f5aa8bf1d58"
 
